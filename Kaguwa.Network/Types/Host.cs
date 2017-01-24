ea779d5b74e292d0d34c7d6e543883f1b439fcf8
Karl Wallenius <karl.wallenius@gmail.com>
2017-01-25 00:49:31 +0100

    Hosts type of object

---
+++ Kaguwa.Network/Types/Host.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Kaguwa.Network.Types
+{
+    public class Host
+    {
+        private string hostName;
+        private IPAddress ipAddress;
+
+        public string HostName
+        {
+            get { return hostName; }
+            set { hostName = value; }
+        }
+
+        public IPAddress IPAddress
+        {
+            get { return ipAddress; }
+            set { ipAddress = value; }
+        }
+
+        public Host() {}
+
+        public Host(string hostName, IPAddress ipAddress)
+        {
+            HostName = hostName;
+            IPAddress = ipAddress;
+        }
+
+        public Host(string hostName, string ipAddress)
+        {
+            HostName = hostName;
+            IPAddress = IPAddress.Parse(ipAddress);
+        }
+    }
+}
