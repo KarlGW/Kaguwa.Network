00ba8e602d9ba2d3f947b1a6e6e8a821d1c82112
Karl Wallenius <karl.wallenius@gmail.com>
2017-02-02 00:34:22 +0100

    Added try catch to handle when th host could not be reached

--- Kaguwa.Network/Dns.cs
+++ Kaguwa.Network/Dns.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Kaguwa.Network.Types;
+﻿using System.Linq;
 using System.Net;
+using Kaguwa.Network.Types;
 
 namespace Kaguwa.Network
 {
@@ -17,12 +13,32 @@ namespace Kaguwa.Network
         /// <returns>Returns a Host object.</returns>
         public static Host GetHost(string hostName)
         {
+            Host host = new Host();
+            string name;
+            IPHostEntry entry;
             // Get the HostEntry.
-            var entry = System.Net.Dns.GetHostEntry(hostName);
-            // Get the IPv4 address.
-            IPAddress ipAddr = entry.AddressList.Where(e => e.AddressFamily.ToString() == "InterNetwork").FirstOrDefault();
+            try
+            {
+                entry = System.Net.Dns.GetHostEntry(hostName);
+                IPAddress ipAddr = entry.AddressList.Where(e => e.AddressFamily.ToString() == "InterNetwork").FirstOrDefault();
+                name = entry.HostName;
+
+                host.HostName = name;
+                host.IPAddress = ipAddr;
+            }
+            catch(System.Net.Sockets.SocketException)
+            {
+                host.HostName = "No such host is known";
+                host.IPAddress = null;
+            }
+            catch(System.Exception)
+            {
+                host.HostName = "IPv4 address 0.0.0.0 and IPv6 address ::0 cannot be used.";
+                host.IPAddress = null;
+            }
+
             // Return a new Host object.
-            return new Host(entry.HostName, ipAddr);
+            return host;
         }
     }
 }
